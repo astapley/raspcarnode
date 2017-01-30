783573b5f781c516d825719a9c35be5374bc3c1b
Austin Stapley <austinstapley@gmail.com>
2017-01-29 22:18:45 -0700

    Add startup script and pi-blaster.js

--- routes/drive.js
+++ routes/drive.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var piblaster = require('pi-blaster.js');
 
 router.get('/', function(req, res, next) {
   res.send('missing params /:throttle/:steering');
 });
 
 router.get('/:throttle/:steering', function(req, res, next) {
-  res.send(req.params);
+  var throttle = req.params.throttle/1000;
+  var steering = req.params.steering/1000;
+  piblaster.setPwm(17, throttle);
+  piblaster.setPwm(18, steering);
+  res.params.throttle = throttle;
+  res.params.steering = steering;
+  res.send(res.params);
 });
 
 module.exports = router;
---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# A simple script
+DEBUG=raspcarnode:* npm start --prefix /home/pi/raspcarnode
